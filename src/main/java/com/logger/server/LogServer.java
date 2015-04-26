60d3feb2e64e624d3eb7226a89f12e0e17c02c60
srinivaspr <srinivasapavankumarreddy@gmail.com>
2015-04-26 11:54:56 -0700

    server change

--- src/main/java/com/logger/server/LogServer.java
+++ src/main/java/com/logger/server/LogServer.java
@@ -3,9 +3,9 @@ package com.logger.server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ltrf.LogMessage;
@@ -15,13 +15,12 @@ public class LogServer {
 	
 	private static Logger logger = LoggerFactory.getLogger(LogServer.class);
 	
-	@RequestMapping("/savelogMessage")
-	public @ResponseBody String saveLogMessage(@PathVariable LogMessage message)
+	@RequestMapping(value="/saveLogMessage",method = RequestMethod.POST)
+	public @ResponseBody void saveLogMessage(@RequestBody LogMessage message)
 	{
-//		logger.info(message.getAppName());
-//		logger.info(message.getKey());
-//		logger.info(message.getMessage());
-		return "i am working";
+		System.out.println(message.getAppName());
+		logger.info(message.getKey());
+		logger.info(message.getMessage());
 	}
-
+	
 }
